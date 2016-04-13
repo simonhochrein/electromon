bbf5a49feaa69215b6364cdefb23d9aab26d81a1
ministrycrm-admin <info@ministrycrm.com>
2016-04-13 13:27:52 -0500

    Commit

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ var child;
 child = proc.spawn(electron,['.']);
 const readline = require('readline');
 
-var settings = require("fs").readFileSync("./.electromonrc",'utf-8');
+var settings = (require("fs").existsSync("./.electromonrc")?require("fs").readFileSync("./.electromonrc",'utf-8'):"");
  watch.watchTree('.',{ignoreDotFiles:true}, function (f, curr, prev) {
     if (typeof f == "object" && prev === null && curr === null) {
       // Finished walking the tree
     } else{
-        if(RegExp(settings.replace(/\n/g,'')).test(f)==false){
-            restart();
+        if(settings!=''&&RegExp(settings.replace(/\n/g,'')).test(f)==false){
+            debounce(restart(),750);
+        } else {
+            debounce(restart(),750);
         }
     }
-  })
-console.log(settings);
+  });
 
 child.stdout.pipe(process.stdout);
 
@@ -44,7 +45,28 @@ function restart(){
     child.stdin.pause();
     child.kill();
       child = proc.spawn(electron,['.']);
-    settings = require("fs").readFileSync("./.electromonrc",'utf-8');
+    settings = (require("fs").existsSync("./.electromonrc")?require("fs").readFileSync("./.electromonrc",'utf-8'):"");
+
 }
+child.on('exit',function(){
+    console.log("Shutting Down Electron");
+    child.stdin.pause();
+    child.kill();
+    process.exit();
+})
  
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 // One-liner for current directory, ignores .dotfiles 
